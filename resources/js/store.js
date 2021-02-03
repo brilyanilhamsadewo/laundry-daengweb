73f116acce6cf38208c68ab52d9762c5d4964b7d
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2021-02-03 09:47:12 +0700

    Part 5 done

--- resources/js/store.js
+++ resources/js/store.js
@@ -5,6 +5,7 @@ import Vuex from 'vuex'
 import auth from './stores/auth.js'
 import outlet from './stores/outlet.js'
 import courier from './stores/courier.js'
+import product from './stores/product.js'
 
 Vue.use(Vuex)
 
@@ -14,7 +15,8 @@ const store = new Vuex.Store({
     modules: {
         auth,
         outlet,
-        courier
+        courier,
+        product
     },
   	//STATE HAMPIR SERUPA DENGAN PROPERTY DATA DARI COMPONENT HANYA SAJA DAPAT DIGUNAKAN SECARA GLOBAL
     state: {
